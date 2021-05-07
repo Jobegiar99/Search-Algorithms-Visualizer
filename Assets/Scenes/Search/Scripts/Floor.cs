e50392c64af383c3a027cf64701e4deef75f320f
José Bernardo Girón Arana <bernardo.giron99@gmail.com>
2021-05-07 00:05:32 -0500

    Add UI and basic scripts

---
+++ Assets/Scenes/Search/Scripts/Floor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string type;
+
+    private void OnMouseDown()
+    {
+        controller control = GameObject.Find("Controller").GetComponent<controller>();
+        GetComponent<SpriteRenderer>().sprite = control.sprites[control.selectedSpriteIndex];
+        type = control.selectedFloorType;
+        if( type == "Goal")
+        {
+            if(control.goal != null){
+                control.goal.GetComponent<SpriteRenderer>().sprite = control.sprites[0];
+                control.goal.GetComponent<Floor>().type = "Normal";
+                control.goal = gameObject;
+            }else
+            {
+                control.goal = gameObject;
+            }
+            
+        }
+
+        if( type == "Start" )
+        {
+            if (control.start != null)
+            {
+                control.start.GetComponent<SpriteRenderer>().sprite = control.sprites[0];
+                control.start.GetComponent<Floor>().type = "Normal";
+                control.start = gameObject;
+            }
+            else
+            {
+                control.start = gameObject;
+            }
+        }
+    }
+    
+}
---
+++ Assets/Scenes/Search/Scripts/controller.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject goal = null;
+    public GameObject start = null;
+
+    public GameObject floor;
+    public List<Sprite> sprites = new List<Sprite>();
+
+    public string selectedFloorType = "Path";
+
+    public int selectedSpriteIndex = 0;
+
+    void Start()
+    {
+        for(int row = -50; row < 51;  row++)
+        {
+            for(int column = -25; column < 26; column++)
+            {
+
+                GameObject tempFloor = Instantiate(floor, new Vector2(row, column), Quaternion.identity);
+                tempFloor.name = row.ToString() + "," + column.ToString();
+
+            }
+        }
+    }
+
+    public void updateSelectedFloorType(string newSelectedFloorType)
+    {
+        selectedFloorType = newSelectedFloorType;
+        handleTypeChange();
+    }
+
+    public void checkSprite()
+    {
+        switch (selectedFloorType )
+        {
+            case "Normal":
+                {
+                    selectedSpriteIndex = 0;
+                }
+                break;
+            case "Path":
+                {
+                    selectedSpriteIndex = 1;
+                }
+                break;
+
+            case "Wall":
+                {
+                    selectedSpriteIndex = 2;
+                }
+                break;
+
+            case "Start":
+                {
+                    selectedSpriteIndex = 3;
+                }
+                break;
+
+            case "Goal":
+                {
+                    selectedSpriteIndex = 4;
+                }
+                break;
+        }
+    }
+
+    void handleTypeChange( )
+    {
+        checkSprite();
+        GameObject.Find("Selected").GetComponent<Image>().sprite = sprites[selectedSpriteIndex];
+    }
+
+}
